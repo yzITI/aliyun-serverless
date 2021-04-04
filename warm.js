b9dc647461ca7df4fe9e34fbd160a3a87eb899de
CutePikachu <z5191511@ad.unsw.edu.au>
2021-04-04 19:43:43 +1000

    modify warm to make it not send msg during nights

--- warm.js
+++ warm.js
@@ -5,5 +5,7 @@ const URL = [
 
 const axios = require('axios')
 setInterval(() => {
+  const h = (new Date()).getHours()
+  if (h < 8 || h >= 22) return
   for (const u of URL) axios.get(u).catch(() => {})
 }, INTERVAL * 60e3)
